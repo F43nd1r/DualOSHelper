ba1db4a2fd41bde6ca849667409c5ef0e9b9ecda
f43nd1r <lukas.morawietz@gmail.com>
2018-12-05 04:09:48 +0100

    rolling log

--- src/main/res/logback.groovy
+++ src/main/res/logback.groovy
@@ -1,9 +1,13 @@
 def USER_HOME = System.getProperty("user.home");
 
-appender("FILE", FileAppender) {
+appender("FILE", RollingFileAppender) {
     file = "$USER_HOME/.clipboardshare/out.log"
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} %-5level %logger{5} - %msg%n"
+    }
+    rollingPolicy(TimeBasedRollingPolicy) {
+        fileNamePattern = "$USER_HOME/.clipboardshare/out-%d{yyyy-MM-dd-HH}.log"
+        maxHistory = 48
     }
 }
 
